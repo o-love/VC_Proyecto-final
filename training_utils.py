16f37779bfe9ffc9e511a5b7d731ec9bae8529b4
Oscar Love <olabradalove@gmail.com>
2023-12-30 13:55:24 +0000

    Quick commit

--- training_utils.py
+++ training_utils.py
@@ -1,30 +1,19 @@
 import pandas as pd
 from IPython.core.display_functions import display
 
+x_col_name = 'image_name'
+y_col_name = 'count'
 
-def load_tri_image_generators(labels_df, dataset_path, batch_size, img_size, dataset_sizes):
-    display(labels_df)
 
-    (train_size, validation_size, test_size) = dataset_sizes
-
-    if (train_size+validation_size+test_size) != len(labels_df):
-        print('Dataset size is different from specified class sizes')
-        exit(1)
-
+def load_generators(training_df, validation_df, dataset_path, batch_size, img_size):
     from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-
-    training_df = labels_df[:train_size]
-    validation_df = labels_df[train_size:train_size+validation_size].reset_index(drop=True)
-    test_df = labels_df[train_size+validation_size:].reset_index(drop=True)
-
-
     datagen = ImageDataGenerator(
-        rescale=1./255,
+        rescale=1. / 255,
     )
 
-    x_col_name = 'image_name'
-    y_col_name = 'count'
+    display(training_df)
+    print(x_col_name)
+    print(y_col_name)
 
     train_generator = datagen.flow_from_dataframe(
         training_df,
@@ -46,12 +35,5 @@ def load_tri_image_generators(labels_df, dataset_path, batch_size, img_size, dat
         batch_size=batch_size,
     )
 
-    test_generator = datagen.flow_from_dataframe(
-        test_df,
-        dataset_path + '/frames/frames/',
-        x_col=x_col_name,
-        y_col=y_col_name,
-        class_mode='raw',
-        target_size=img_size,
-        batch_size=batch_size,
-    )
+    return (train_generator, validation_generator)
+
